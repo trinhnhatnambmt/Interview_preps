1c2a1540ffb2db10bb6f272ed69276bbdc033631
Huy Trịnh <trinhnhathuy3@gmail.com>
2025-08-05 10:28:20 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+};
+
+module.exports = {
+    extends: [
+        "next",
+        "next/core-web-vitals",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    rules: {
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
+        "@typescript-eslint/no-unused-vars": [
+            "warn",
+            { argsIgnorePattern: "^_" },
+        ],
+    },
 };
 
 export default nextConfig;
